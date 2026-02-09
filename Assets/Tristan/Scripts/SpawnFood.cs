56a157d42e7bf26c611e7e1871d8d398b3bd7702
Clem492 <c.turpin@rubika-edu.re>
2026-02-09 12:30:49 +0400

    prefab ajouter correctement

--- Assets/Tristan/Scripts/SpawnFood.cs
+++ Assets/Tristan/Scripts/SpawnFood.cs
@@ -5,18 +5,26 @@ public class SpawnFood : MonoBehaviour
 {
     [SerializeField] Cuisine cuisine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, new Vector3(0, -25, 0), Color.red);
+    }
 
     public void Spawn(GameObject go , Stack<GameObject> pileGo)
     {
         RaycastHit hit;
 
-        Debug.DrawRay(transform.position, new Vector3(0, -25, 0), Color.red);
+       
         Physics.Raycast(transform.position, Vector3.down, out hit, 25);
         
         if(go.CompareTag("Pain") && cuisine.sensPain == true)
         {
-            pileGo.Push(Instantiate(go, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.Euler(180, 0, 0)));
+            pileGo.Push(Instantiate(go, hit.point, Quaternion.Euler(180, 0, 0)));
+        }
+        if (go.CompareTag("Viande"))
+        {
+            pileGo.Push(Instantiate(go, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.Euler(-90, 0, 0)));
         }
         else
         {
