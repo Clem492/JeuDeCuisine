cab11800bfb5a28f107ef64972c5d9314c2472a1
Tristan <t.laurent@rubika-edu.re>
2026-02-10 15:09:16 +0400

    fin fonctionnelle

--- Assets/Tristan/Scripts/MenuScripts/RetourMenu.cs
+++ Assets/Tristan/Scripts/MenuScripts/RetourMenu.cs
@@ -1,16 +1,21 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RetourMenu : MonoBehaviour
 {
+    private string Menu = "Menu";
+    [SerializeField] float time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(Retour());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Retour()
     {
-        
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(Menu);
     }
+    
 }
